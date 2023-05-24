44f9eef6c5ebe93ab93244710cac1b1e457f2ade
Yoo SeonHo <tjsgh503@naver.com>
2023-05-25 06:48:49 +0900

    feat : 3-2 등록된 채용 정보 조회 설명 주석

--- Homework3-2/Recruit/ShowRecruit.cpp
+++ Homework3-2/Recruit/ShowRecruit.cpp
@@ -14,3 +14,12 @@ vector<Recruit *> ShowRecruit::showRecruitList(string companyNumber)
 
     return Rlist;
 }
+
+/*  1. showRecruit 매개변수 넣어서 생성자로 호출
+    2. UI startInterface 실행되고
+    3. boundary 실행
+    4. control 실행
+    5. RecruitCollection에서 companyNumber가 같은 애들 꺼 반환
+    6. control에서 받아서 boundary로 반환
+    7. boundary에서 반환값 출력
+*/
