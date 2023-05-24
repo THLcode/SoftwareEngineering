212526d7af1c8145f4291c608c00aeec31e213b1
Yoo SeonHo <tjsgh503@naver.com>
2023-05-24 13:35:49 +0900

    fix : header로 모음

--- Homework3/implementation/header.h
+++ Homework3/implementation/header.h
@@ -1,1 +1,60 @@
-//test
+#include <iostream>
+#include <vector>
+#include <tuple>
+using namespace std;
+
+class Recruit
+{
+private:
+    string job;
+    int peopleNum;
+    string dueDate;
+    string companyNumber;
+
+public:
+    tuple<string, int, string, string> getRecruitDetails(string);
+};
+
+class RecruitInfoCollection
+{
+private:
+    vector<Recruit> recruitInfo;
+
+public:
+    vector<Recruit> getRecruitList();
+    void addRecruit(Recruit);
+};
+
+class ApplyInfoCollection
+{
+private:
+    vector<Recruit> applyInfo;
+
+public:
+    string cancelApplyInfo(Recruit);
+    void addApplyInfo(Recruit);
+    vector<Recruit> getApplyInfoList();
+};
+
+class CompanyUser
+{
+private:
+    string companyName;
+    int companyNumber;
+    RecruitInfoCollection *recruitInfoCollection;
+
+public:
+    RecruitInfoCollection *getRecruitInfoCollection();
+    vector<pair<string, int>> getCompanyUserDetails();
+};
+
+class NormalUser
+{
+private:
+    string normalName;
+    int securityNumber;
+    ApplyInfoCollection *applyInfoCollection;
+
+public:
+    ApplyInfoCollection *getApplyInfoCollection();
+};
